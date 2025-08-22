bdd602d9b5be3ee90002fee05e1f28b32742daca
saquib-sarfaraz <saquibsarfaraz47@gmail.com>
2025-08-22 11:10:26 +0530

    Updated app.js: fixed winner logic and reset button

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ const resetGame = () => {
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         if(turnO){
-            box.innerText ="O";
+            box.innerText ="X";
             turnO = false;
         } else {
-            box.innerText ="X";
+            box.innerText ="O";
             turnO = true;
         }
         box.disabled = true;
